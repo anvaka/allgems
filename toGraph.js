43f13b665bcf148f3a293cdbae7f9c817784161a
anvaka <anvaka@gmail.com>
2015-05-07 23:47:16 -0700

    Added toGraph converter

---
+++ toGraph.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var inputFileName = process.argv[2] || './gems_bk.json';
+var graph = require('ngraph.graph')({uniqueLinkId: false});
+var tojson = require('ngraph.tojson');
+var emptyArray = [];
+
+var packages = require(inputFileName);
+for (var i = 0; i < packages.length; ++i) {
+  var pkg = packages[i];
+  var source = pkg.name;
+  graph.addNode(source);
+  var targets = getDeps(pkg);
+  for (var j = 0; j < targets.length ; j++) {
+    var target = targets[j].name;
+    if (target) graph.addLink(source, target);
+  }
+}
+
+fs.writeFileSync('graph.out', tojson(graph), 'utf8');
+console.log('Saved '  + graph.getNodesCount() + ' nodes; ' + graph.getLinksCount() + ' edges');
+
+function getDeps(pkg) {
+  return (pkg.dependencies && pkg.dependencies.runtime) || emptyArray;
+}
+
