4e6c0bc329021fc97f84bc37eff76f499ff30d2e
anvaka <anvaka@gmail.com>
2015-06-28 18:48:35 -0700

    Using ngraph modules

--- layout.js
+++ layout.js
@@ -1,40 +1,8 @@
-var fs = require('fs');
-var path = require('path');
 var loadGraph = require('./lib/loadGraph.js');
 var graph = loadGraph(process.argv[2] || 'graph.out');
-var layout = require('ngraph.forcelayout3d')(graph);
 
-var i = 0;
-console.log('Starting layout. This will take ~60-120 minutes');
-
-while (i < 501) {
-  console.log('Step ' + i);
-  layout.step();
-  if (i % 5 === 0) {
-    saveIteration(Math.round(i/5));
-  }
-  ++i;
-}
-
-console.log('Done. Convert results to binary format now:');
-console.log(' node toBinary.js ./graph.out');
-
-function saveIteration(name) {
-  var fname = path.join('data', name + '.pos');
-
-  console.log("Saving: ", fname);
-  var nodesLength = graph.getNodesCount();
-  var buf = new Buffer(nodesLength * 4 * 3);
-  var i = 0;
-
-  graph.forEachNode(function (node) {
-    var idx = i * 4 * 3;
-    var pos = layout.getNodePosition(node.id);
-    buf.writeInt32LE(pos.x, idx);
-    buf.writeInt32LE(pos.y, idx + 4);
-    buf.writeInt32LE(pos.z, idx + 8);
-    i++;
-  });
-
-  fs.writeFileSync(fname, buf);
-}
+var layout = require('ngraph.offline.layout')(graph);
+console.log('Starting layout. This will take a while...');
+layout.run();
+console.log('Done. Now export this to binary format:');
+console.log('node toBinary.js');
